e9a57aff0e5200b2f42eb8b320994b2daa638864
Daniel Clarke <deliotclarke@gmail.com>
2020-07-12 11:10:20 -0500

    got it deployed to azure. no persistent databse, but it does work. can't get docker debugger to work locally without hardcoding the secret key for some reason.

--- manage.py
+++ manage.py
@@ -2,10 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_js_to_py.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'project_js_to_py.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
